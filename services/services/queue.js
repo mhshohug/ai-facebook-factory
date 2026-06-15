a5b2f90858f486ced1108b8eade256935ee815d0
mhshohug <mh.shohug@gmail.com>
2026-06-15 18:06:44 +0600

    Update queue.js

--- services/services/queue.js
+++ services/services/queue.js
@@ -1,1 +1,30 @@
+class QueueService {
 
+    constructor() {
+        this.jobs = [];
+    }
+
+    add(job) {
+        this.jobs.push(job);
+        return this.jobs.length;
+    }
+
+    next() {
+        return this.jobs.shift();
+    }
+
+    list() {
+        return this.jobs;
+    }
+
+    size() {
+        return this.jobs.length;
+    }
+
+    clear() {
+        this.jobs = [];
+    }
+
+}
+
+module.exports = new QueueService();

b2249b2b24835485ae9640e6b393d4a97689b71d
mhshohug <mh.shohug@gmail.com>
2026-06-15 21:57:24 +0600

    Update voice.js

--- services/voice.js
+++ services/voice.js
@@ -1,1 +1,34 @@
+const logger = require("./logger");
 
+class VoiceService {
+
+    async generate(script) {
+
+        try {
+
+            logger.info("Voice Generation Started");
+
+            // পরে এখানে Edge TTS বা Google TTS লাগাব
+
+            return {
+                success: true,
+                file: "output/voice.mp3",
+                message: "Voice placeholder created"
+            };
+
+        } catch (err) {
+
+            logger.error(err.message);
+
+            return {
+                success: false,
+                error: err.message
+            };
+
+        }
+
+    }
+
+}
+
+module.exports = new VoiceService();

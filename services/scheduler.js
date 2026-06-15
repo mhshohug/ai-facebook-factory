cf3c7cd458f0d44379ea984e994bed62efe0a6bc
mhshohug <mh.shohug@gmail.com>
2026-06-15 17:30:23 +0600

    Update scheduler.js

--- services/scheduler.js
+++ services/scheduler.js
@@ -1,1 +1,23 @@
+const cron = require("node-cron");
+const logger = require("./logger");
 
+class Scheduler {
+
+    start() {
+
+        logger.info("Scheduler Started");
+
+        // প্রতিদিন সকাল ৯:০০ টায় চলবে
+        cron.schedule("0 9 * * *", async () => {
+
+            logger.info("Running Daily AI Automation...");
+
+            // পরে এখানে AI Script + Video + Facebook Post হবে
+
+        });
+
+    }
+
+}
+
+module.exports = new Scheduler();

46f845c9f944e9ebf70bc113f702f91d6f6fb116
mhshohug <mh.shohug@gmail.com>
2026-06-15 17:22:38 +0600

    Update logger.js

--- services/logger.js
+++ services/logger.js
@@ -1,1 +1,42 @@
+const fs = require("fs");
+const path = require("path");
 
+class Logger {
+
+    constructor() {
+        this.logDir = path.join(__dirname, "../logs");
+
+        if (!fs.existsSync(this.logDir)) {
+            fs.mkdirSync(this.logDir, { recursive: true });
+        }
+    }
+
+    write(level, message) {
+
+        const now = new Date().toISOString();
+
+        const log = `[${now}] [${level}] ${message}\n`;
+
+        console.log(log);
+
+        fs.appendFileSync(
+            path.join(this.logDir, "app.log"),
+            log
+        );
+    }
+
+    info(message) {
+        this.write("INFO", message);
+    }
+
+    warn(message) {
+        this.write("WARN", message);
+    }
+
+    error(message) {
+        this.write("ERROR", message);
+    }
+
+}
+
+module.exports = new Logger();
